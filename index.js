77a2d1bc4b546b0b52dbb3467b0fd9e24fba69db
Zeke Sikelianos <zeke@sikelianos.com>
2024-09-24 15:37:30 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import Replicate from 'replicate'
 import download from 'download'
-import { readFile, mkdir } from 'node:fs/promises'
+import { readFile, mkdir, rm } from 'node:fs/promises'
 import path from 'path'
 import { exec } from 'child_process'
 import { promisify } from 'util'
@@ -13,14 +13,23 @@ const input = {
   image: await readFile(path.resolve(process.cwd(), 'zeke.jpg'))
 }
 
-// Create outputs directory
+// Remove existing outputs
 const outputsDir = path.join(process.cwd(), 'outputs')
+await rm(outputsDir, { recursive: true, force: true })
+
+// (Re)create outputs directory
 await mkdir(outputsDir, { recursive: true })
 
+const maxWink = 25
+const maxSmile = 1.3
+const iterations = 25
+
 // Generate images
-for (let i = 0; i < 25; i++) {
-  input.wink = i
-  console.log('wink', i)
+for (let i = 0; i < iterations; i++) {
+  input.wink = maxWink * i / iterations
+  input.smile = maxSmile * i / iterations
+  console.log(i, { input })
+
   const output = await replicate.run(model, { input })
   console.log({ output })
   console.log('\n\n')
@@ -32,7 +41,7 @@ for (let i = 0; i < 25; i++) {
 async function createVideo () {
   const inputPattern = path.join(outputsDir, 'output-%02d.webp')
   const outputFile = path.join(process.cwd(), 'output.mp4')
-  const ffmpegCommand = `ffmpeg -framerate 10 -i "${inputPattern}" -filter_complex "[0:v]reverse[r];[0:v][r]concat,loop=1:250,setpts=N/25/TB" -c:v libx264 -pix_fmt yuv420p ${outputFile}`
+  const ffmpegCommand = `ffmpeg -framerate 20 -i "${inputPattern}" -filter_complex "[0:v]reverse[r];[0:v][r]concat,loop=1:250,setpts=N/50/TB" -c:v libx264 -pix_fmt yuv420p ${outputFile}`
 
   console.log('Starting video creation with ffmpeg...')
 
